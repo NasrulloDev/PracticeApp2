b48a2ce36e263d904d9d2e37734c11bd722aeb55
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-02-25 14:20:45 +0500

    Initial Commit

---
+++ PracticeApp2/PracticeApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp2App.swift
+//  PracticeApp2
+//
+//  Created by Насрулло Исмоилжонов on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
